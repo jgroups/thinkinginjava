330e7fe206ec7140343520367f0d9a0c501d46f8
JeremyKing <jgroups@163.com>
2016-05-11 17:16:11 +0800

    me.lbing.generic

---
+++ src/me/lbing/generic/Holder3.java
@@ -0,0 +1,25 @@
+package me.lbing.generic;
+
+/**
+ * Created by King on 2016/5/11.
+ */
+public class Holder3<T> {
+    private T a;
+
+    public Holder3(T a) {
+        this.a = a;
+    }
+
+    public T getA() {
+        return a;
+    }
+
+    public void setA(T a) {
+        this.a = a;
+    }
+
+    public static void main(String[] args) {
+        Holder3<String> h = new Holder3("");
+        String a = h.getA();
+    }
+}
