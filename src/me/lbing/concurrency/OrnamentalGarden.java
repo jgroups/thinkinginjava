8d90b71c437f5134d629b7c571bb6b58c31f31ab
JeremyKing <jgroups@163.com>
2016-05-21 01:27:14 +0800

    java concurrency interrupt

--- src/me/lbing/concurrency/OrnamentalGarden.java
+++ src/me/lbing/concurrency/OrnamentalGarden.java
@@ -1,7 +1,11 @@
 package me.lbing.concurrency;
 
+import java.util.concurrent.ExecutorService;
+
 /**
  * Created by king on 16/5/20.
  */
 public class OrnamentalGarden {
+    public static void main(String[] args) {
+    }
 }
