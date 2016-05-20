8b298ffe08aa12dd41451a5796abb291e6c4c9cd
JeremyKing <jgroups@163.com>
2016-05-21 01:44:45 +0800

    java concurrency interrupt

---
+++ src/me/lbing/concurrency/CloseResource.java
@@ -0,0 +1,20 @@
+package me.lbing.concurrency;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by king on 16/5/21.
+ */
+public class CloseResource {
+    public static void main(String[] args) throws Exception {
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.execute(new IOBlocked(System.in));
+        Thread.sleep(100);
+        service.shutdown();
+        System.in.close();
+    }
+}
